24ab25d0866f7c18ced3c04f7027d529913a1d8d
BastianAhumadaAguirre <b.ahumadaa@duocuc.cl>
2026-05-04 15:06:39 -0400

    fix(layout): Se ajusta el ancho y se centra AuthLayout.

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,8 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 export const AuthLayout = () => {
   return (
-    <div className='min-h-screen bg-gray-50 flex flex-col'>
-      <Outlet />
+    <div className='min-h-screen bg-gray-50 flex items-center justify-center px-4'>
+      <div className='w-full max-w-md'>
+        <Outlet />
+      </div>
     </div>
   );
 };
