d86882864e46f0488b5ddc8aaa211781e9e49d68
Benjamin Marfull <benja.marfull@gmail.com>
2026-05-11 10:00:42 -0400

    se modifica puerto en el cual se lanza el proyecto y se conecta al MS

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,3 +1,3 @@
 export const PACIENTE_API_URL = import.meta.env.VITE_PACIENTE_API_URL || 'http://localhost:8081/api';
 export const MEDICO_API_URL   = import.meta.env.VITE_MEDICO_API_URL   || 'http://localhost:8082/api';
-export const LISTA_API_URL    = import.meta.env.VITE_LISTA_API_URL    || 'http://localhost:8083/api';
+export const LISTA_API_URL    = import.meta.env.VITE_LISTA_API_URL    || 'http://localhost:8080/api';
